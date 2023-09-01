cfed54b88bad2a348f383a7ad7bb82f0470b69a1
RedstoneParadox <spinjitzu00@gmail.com>
2023-08-31 17:06:58 -0700

    Complete initial setup

---
+++ src/main/java/io/github/redstoneparadox/datapackprofessions/DatapackProfessions.java
@@ -0,0 +1,13 @@
+package io.github.redstoneparadox.datapackprofessions;
+
+import org.quiltmc.loader.api.ModContainer;
+import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DatapackProfessions implements ModInitializer {
+	public static final Logger LOGGER = LoggerFactory.getLogger("Datapack Professions");
+	@Override
+	public void onInitialize(ModContainer mod) {
+	}
+}
