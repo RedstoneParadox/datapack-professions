d934659f89ae8f37fb005b81d25a9f4b6e8cb240
RedstoneParadox <spinjitzu00@gmail.com>
2023-09-04 16:32:58 -0700

    command for dumping trades to json

--- src/main/java/io/github/redstoneparadox/datapackprofessions/DatapackProfessions.java
+++ src/main/java/io/github/redstoneparadox/datapackprofessions/DatapackProfessions.java
@@ -1,12 +1,16 @@
 package io.github.redstoneparadox.datapackprofessions;
 
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import io.github.redstoneparadox.datapackprofessions.command.DumpTradesCommand;
 import io.github.redstoneparadox.datapackprofessions.data.TradeTableReloader;
 import io.github.redstoneparadox.datapackprofessions.trades.TradeOfferFactories;
 import io.github.redstoneparadox.datapackprofessions.trades.TradeOfferFactoryType;
 import net.minecraft.resource.ResourceType;
+import net.minecraft.server.command.CommandManager;
 import net.minecraft.util.Identifier;
 import org.quiltmc.loader.api.ModContainer;
 import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
+import org.quiltmc.qsl.command.api.CommandRegistrationCallback;
 import org.quiltmc.qsl.resource.loader.api.ResourceLoader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,6 +26,14 @@ public class DatapackProfessions implements ModInitializer {
 		var loader = ResourceLoader.get(ResourceType.SERVER_DATA);
 
 		loader.registerReloader(new TradeTableReloader());
+
+		CommandRegistrationCallback.EVENT.register(((dispatcher, buildContext, environment) -> {
+			dispatcher.register(
+				CommandManager.literal("trades").then(
+					CommandManager.literal("dump").executes(new DumpTradesCommand())
+				)
+			);
+		}));
 	}
 
 	public static Identifier id(String path) {
---
+++ src/main/java/io/github/redstoneparadox/datapackprofessions/command/DumpTradesCommand.java
@@ -0,0 +1,81 @@
+package io.github.redstoneparadox.datapackprofessions.command;
+
+import com.google.gson.internal.Streams;
+import com.google.gson.stream.JsonWriter;
+import com.mojang.brigadier.Command;
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import com.mojang.serialization.JsonOps;
+import io.github.redstoneparadox.datapackprofessions.trades.ExtendedTradeOfferFactory;
+import io.github.redstoneparadox.datapackprofessions.trades.TradeTable;
+import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.village.TradeOffers;
+import net.minecraft.village.VillagerProfession;
+import org.quiltmc.loader.api.QuiltLoader;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class DumpTradesCommand implements Command<ServerCommandSource> {
+	@Override
+	public int run(CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
+		for (Map.Entry<VillagerProfession, Int2ObjectMap<TradeOffers.Factory[]>> entry : TradeOffers.PROFESSION_TO_LEVELED_TRADE.entrySet()) {
+			String name = entry.getKey().name();
+			String prefix = name.substring(name.indexOf(':') + 1);
+			Int2ObjectMap<TradeOffers.Factory[]> factoryMap = entry.getValue();
+
+			for (int i = 1; i <= 5; i++) {
+				TradeOffers.Factory[] factories = factoryMap.get(i);
+				List<ExtendedTradeOfferFactory> offers = new ArrayList<>();
+
+				for (TradeOffers.Factory factory : factories) {
+					offers.add((ExtendedTradeOfferFactory) factory);
+				}
+
+				TradeTable table = new TradeTable(false, offers);
+				String levelName = switch (i) {
+					case 2 -> "apprentice";
+					case 3 -> "journeyman";
+					case 4 -> "master";
+					case 5 -> "expert";
+					default -> "novice";
+				};
+				var result = TradeTable.CODEC.encodeStart(JsonOps.INSTANCE, table).result();
+				File directory = new File(QuiltLoader.getGameDir().toFile(), "dumped-trades");
+				File file = new File(directory, prefix + "_" + levelName + ".json");
+
+				result.ifPresent(json -> {
+					try {
+						var exists = true;
+
+						if (!directory.exists()) {
+							directory.mkdir();
+						}
+
+						if (!file.exists()) {
+							exists = file.createNewFile();
+						}
+
+						if (exists) {
+							var jsonWriter = new JsonWriter(new BufferedWriter(new FileWriter(file)));
+							jsonWriter.setLenient(true);
+							Streams.write(json, jsonWriter);
+							jsonWriter.close();
+						}
+
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				});
+			}
+		}
+
+		return 0;
+	}
+}
