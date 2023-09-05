acf8420a270f38ff3786c3b6db15d6af530be398
RedstoneParadox <spinjitzu00@gmail.com>
2023-09-04 17:12:40 -0700

    Setup test mod

---
+++ src/testmod/java/io/github/redstoneparadox/datapackprofessions/test/DatapackProfessionsTest.java
@@ -0,0 +1,11 @@
+package io.github.redstoneparadox.datapackprofessions.test;
+
+import org.quiltmc.loader.api.ModContainer;
+import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
+
+public class DatapackProfessionsTest implements ModInitializer {
+	@Override
+	public void onInitialize(ModContainer mod) {
+
+	}
+}
